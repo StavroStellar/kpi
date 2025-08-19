7f13345056accba0c79733f288ccb0e7ca9d753c
Болгов Лев Николаевич <username@example.com>
2025-08-19 16:49:28 +0300

    Рефакторинг

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template  # ← добавь render_template
+from flask import Flask, render_template
 from app.extensions import db, login_manager
 from app.config import config
 import os
